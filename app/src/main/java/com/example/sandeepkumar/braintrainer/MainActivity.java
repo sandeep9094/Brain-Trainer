aba9ff6958e06bbf3157119f7274c9162a2d5e13
Sandeep Kumar <sandeepkumar9094@gmail.com>
2018-08-08 22:45:46 +0530

    Correct Answer Logic

--- app/src/main/java/com/example/sandeepkumar/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/sandeepkumar/braintrainer/MainActivity.java
@@ -1,26 +1,58 @@
 package com.example.sandeepkumar.braintrainer;
 
+import android.annotation.SuppressLint;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button startButton, ans1, ans2, ans3, ans4;
+    Button startButton, ans1, ans2, ans3, ans4, playAgainButton;
     TextView sumTextView,pointsTextView,resultTextView,timerTextView;
-    ArrayList<Integer> answers = new ArrayList<Integer>();
+    ArrayList<Integer> answers = new ArrayList<>();
     int correctAnsLocation;
     int score = 0;
     int totalQuestions = 0;
     CountDownTimer timer;
+    ConstraintLayout gameLayout;
 
+    @SuppressLint("SetTextI18n")
+    public void playAgain(View view){
+        score = 0;
+        totalQuestions = 0;
+
+        timerTextView.setText("0s");
+        pointsTextView.setText("0/0");
+        resultTextView.setText("");
+        playAgainButton.setVisibility(View.INVISIBLE);
+
+        generateQuetions();
+
+        //total time of clock is 30 seconds = 30,000 milli seconds
+        //Tick ever second i.e. 1 second =1000 milli seconds
+        timer = new CountDownTimer(30000,1000) {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onTick(long l) {
+                timerTextView.setText(String .valueOf(l/1000) + "s");
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Your Score :" + Integer.toString(score)+ "/" + Integer.toString(totalQuestions));
+                playAgainButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
+
+    @SuppressLint("SetTextI18n")
     public void generateQuetions(){
         Random rand = new Random();                             //Creating Random Class Object
 
@@ -52,8 +84,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(ConstraintLayout.VISIBLE);
     }
 
+    @SuppressLint("SetTextI18n")
     public void chooseAns(View view){
         if((view.getTag().toString()).equals(Integer.toString(correctAnsLocation))){
            score++;
@@ -71,30 +106,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ans1 = (Button)findViewById(R.id.ans1);
-        ans2 = (Button)findViewById(R.id.ans2);
-        ans3 = (Button)findViewById(R.id.ans3);
-        ans4 = (Button)findViewById(R.id.ans4);
+        ans1 = findViewById(R.id.ans1);
+        ans2 = findViewById(R.id.ans2);
+        ans3 = findViewById(R.id.ans3);
+        ans4 = findViewById(R.id.ans4);
 
-        startButton = (Button) findViewById(R.id.startButton);
-        sumTextView = (TextView) findViewById(R.id.sumTextView);
-        pointsTextView = (TextView) findViewById(R.id.pointsTextView);
-        resultTextView = (TextView) findViewById(R.id.resultTextView);
-        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        playAgainButton = findViewById(R.id.playAgainButton);
+        sumTextView = findViewById(R.id.sumTextView);
+        pointsTextView = findViewById(R.id.pointsTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        timerTextView = findViewById(R.id.timerTextView);
+        gameLayout = findViewById(R.id.gameLayout);
 
-        generateQuetions();
-        //total time of clock is 30 seconds = 30,000 milli seconds
-        //Tick ever second i.e. 1 second =1000 milli seconds
-        timer = new CountDownTimer(30000,1000) {
-            @Override
-            public void onTick(long l) {
-                timerTextView.setText(String .valueOf(l/1000));
-            }
+        playAgain(timerTextView);
 
-            @Override
-            public void onFinish() {
-                resultTextView.setText("Done");
-            }
-        }.start();
     }
 }
